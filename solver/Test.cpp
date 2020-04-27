2ad0fdc2fc47ba56592d865b25ad65356d775580
bhori <bhorib@gmail.com>
2020-04-27 18:14:13 +0300

    temp

--- solver/Test.cpp
+++ solver/Test.cpp
@@ -7,16 +7,10 @@ using namespace std;
 
 TEST_CASE("Test addMother")
 {
-  family::Tree t("ori");
-  t.addMother("ori", "hana");
-  t.addMother("hana", "sara");
-  t.addMother("sara", "miriam");
-  t.addMother("miriam", "rivka");
-  t.addMother("rivka", "hadas");
-  CHECK(t.find("mother") == string("yosef"));
-  CHECK(t.find("grandmother") == string("yitzhak"));
-  CHECK(t.find("great-grandmother") == string("yaakov"));
-  CHECK(t.find("great-great-grandmother") == string("david"));
-  CHECK(t.find("great-great-great-grandmother") == string("nisim"));
-//  delete &t;
+  RealVariable x;
+  CHECK(solve(2*x-4==10) == 7);
+  CHECK(solve(2*x-4==10) == 7);
+  CHECK(solve(2*x-4==10) == 7);
+  CHECK(solve(2*x-4==10) == 7);
+  CHECK(solve(2*x-4==10) == 7);
 }
