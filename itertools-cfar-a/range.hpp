79d77991f5bca80f4e4c845b0a44fece5d6d065f
Ori <bhorib@gmail.com>
2020-06-17 12:36:20 +0300

    add accumulate

--- itertools-cfar-a/range.hpp
+++ itertools-cfar-a/range.hpp
@@ -62,4 +62,101 @@ namespace itertools{
 		return iterator{ends};
 	}
     };
+
+
+
+
+	template<typename T>
+    class accumulate{
+        // decltype(*(typename T::iterator)) begins;
+        // decltype(*(typename T::iterator)) ends;
+		T& container;
+		decltype(*(begin(container))) begins;
+		// decltype(typename T::iterator_traits<typename T::iterator>) begins;
+		decltype(*(end(container))) ends;
+        // typename iterator_traits<typename T::template::iterator>::value_type ends;
+        // T& container;
+
+    public:
+        accumulate(T m): container(m), begins{*(m.begin())}, ends{*(m.end())}{
+			// begins=*(m.begin());
+			// ends=*(m.end());
+		}
+
+private:
+
+    //-------------------------------------------------------------------
+	// iterator related code:
+	// inner class and methods that return instances of it)
+	//-------------------------------------------------------------------
+	class iterator {
+
+	  private:
+		typename T::iterator itr;
+        decltype(*(itr)) value;
+
+	public:
+
+		iterator(T m)
+			: itr(m.begin()), value(*itr) {
+                // value=*itr;
+		}
+
+        // iterator(T m)
+		// 	: itr(m) {
+        //         value=*itr;
+		// }
+
+		// Note that the method is const as this operator does not
+		// allow changing of the iterator.
+		// Note that it returns T& as it allows to change what it points to.
+		// A const_iterator class will return const T&
+		// and the method will still be const
+		auto operator*() const {
+			//return *pointer_to_current_node;
+			return value;
+		}
+
+		// T* operator->() const {
+		// 	return &(pointer_to_current_node->m_value);
+		// }
+
+		// ++i;
+		iterator& operator++() {
+			//++pointer_to_current_node;
+            itr++;
+            value=value+*itr;
+			// pointer_to_current_node = pointer_to_current_node->m_next;
+			return *this;
+		}
+
+		// i++;
+		// Usually iterators are passed by value and not by const& as they are small.
+		const iterator operator++(int) {
+			iterator tmp= *this;
+			itr++;
+            value=value+*itr;
+            // pointer_to_current_node= pointer_to_current_node->m_next;
+			return tmp;
+		}
+
+		bool operator==(const iterator& rhs) const {
+			return value == rhs.value;
+		}
+
+		bool operator!=(const iterator& rhs) const {
+			return value != rhs.value;
+		}
+	};  // END OF CLASS ITERATOR
+
+public:
+
+	iterator begin() {
+		return iterator(container);
+	}
+	
+	iterator end() {
+		return iterator{container};
+	}
+    };
 }
